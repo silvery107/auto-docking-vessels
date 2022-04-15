c46989510ded78a62dbacccf6bbef360d76dc4f7
silvery107 <1079931505@qq.com>
2022-04-15 20:27:32 +0800

    update basic controller

--- controllers/UAV_controller/UAV_controller.py
+++ controllers/UAV_controller/UAV_controller.py
@@ -3,6 +3,8 @@
 from controller import Robot
 import numpy as np
 import cv2
+from gamepad_reader import Gamepad
+from locomotion import Locomotion
 
 # TODO
 # 1. 换模型
@@ -33,8 +35,6 @@ for name in motor_name:
     motor.setVelocity(0.0)
     motors.append(robot.getDevice(name))
 
-# Camera
-camera = sensor_builder("camera", timestep)  # (240, 320, 3)
 
 # GPS
 gps = sensor_builder("gps", timestep)
@@ -45,24 +45,32 @@ gyro = sensor_builder("gyro", timestep)
 # Inertial Unit
 inertial_unit = sensor_builder("inertia_unit", timestep)
 
+# Camera
+camera = sensor_builder("camera", timestep)  # (240, 320, 3)
+K = np.array([265, 0, 240, 0, 265, 160, 0, 0, 1]).reshape((3,3))
+distCoeffs = np.zeros(5)
 ARUCO_TAG = cv2.aruco.DICT_6X6_50
 this_aruco_dictionary = cv2.aruco.Dictionary_get(ARUCO_TAG)
 this_aruco_parameters = cv2.aruco.DetectorParameters_create()
 
-K = np.array([265, 0, 240, 0, 265, 160, 0, 0, 1]).reshape((3,3))
-distCoeffs = np.zeros(5)
+# Gamepad
+gamepad = Gamepad(1, 1, 1)
+locomotion = Locomotion(robot, motors)
+
 # Main loop:
-# - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
 
-    # Process sensor data here.
-    for motor in motors:
-        motor.setVelocity(-0.1)
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
+    lin_speed, ang_speed, e_stop = gamepad.get_command()
+    locomotion.forward(lin_speed[0])
+    locomotion.moveRight(lin_speed[1])
+    locomotion.turnLeft(ang_speed)
+
+    # for motor in motors:
+    #     motor.setVelocity(-0.1)
+        
     image = np.array(camera.getImageArray()).astype(np.uint8)  # (320, 480, 3)
     image = cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_90_CLOCKWISE)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR).astype(np.uint8)
@@ -71,12 +79,13 @@ while robot.step(timestep) != -1:
     #print(corners)
 
     if len(corners) > 0:
+        cv2.aruco.drawDetectedMarkers(image, corners, ids)
         rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 0.03, K, distCoeffs)
         #K, distCoeffs = cv2.aruco.calibrateCameraAruco(corners, ids, 3, board, (480, 320,))
-        cv2.aruco.drawDetectedMarkers(image, corners, ids)
 
             
-    cv2.imshow("window", image)
-    cv2.waitKey(1)
+    # cv2.imshow("window", image)
+    # cv2.waitKey(1)
 
+gamepad.stop()
 # Enter here exit cleanup code.
