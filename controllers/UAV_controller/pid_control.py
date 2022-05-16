2d3f749ccd3fd41a02b2e28c03764360dd2bcec7
SilverSoul <1079931505@qq.com>
2022-05-17 01:28:01 +0800

    add pid controller

---
+++ controllers/UAV_controller/pid_control.py
@@ -0,0 +1,17 @@
+class PID_Controller:
+    def __init__(self, Kp=0.0, Kd=0.0, Ki=0.0):
+        self.Kp = float(Kp)
+        self.Kd = float(Kd)
+        self.Ki = float(Ki)
+        self.err_last = 0.0
+        self.err_int = 0.0
+    
+    def apply(self, err):
+        self.err_int += err
+        u = self.Kp * err + self.Kd * (err-self.err_last) + self.Ki * self.err_int
+        self.err_last = err
+        return u
+
+    def reset(self):
+        self.err_int = 0.0
+        self.err_last = 0.0
